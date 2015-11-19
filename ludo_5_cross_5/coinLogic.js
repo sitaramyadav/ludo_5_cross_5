900aa709aa613693a6e720c2a67b93e42e278208
vathsala <vathsala@INvathsala.local>
2015-11-19 14:49:37 +0530

    added coin Logic file

---
+++ ludo_5_cross_5/coinLogic.js
@@ -0,0 +1,18 @@
+var lib = {};
+exports.lib = lib;
+
+lib.Coin = function(colour,id){
+	this.colour = colour;
+	this.position = [0,0];
+	this.id = id;
+}
+lib.Coin.prototype = {
+	previousPosition : [0,0],
+	move : function(finalPosition){
+		this.previousPosition = this.position;
+		this.position = finalPosition; 
+	},
+	die : function(){
+		this.position = [0,0];
+	}
+}
