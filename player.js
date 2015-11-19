2dad63fb3438fa27bd27c363c751f9e7c7f49294
Sarath Vs <sarathvss65@gmail.com>
2015-11-19 14:54:07 +0530

    added player.js

---
+++ player.js
@@ -0,0 +1,23 @@
+var Coin = require('coin.js').Coin;
+
+var generateCoins = function(color){
+	var coins = [];
+	for (var i = 0; i < 4; i++) 
+		coins.push(new Coin(color,i));
+	return coins;
+}
+
+var Player = function(name,color){
+	this.name = name;
+	this.color = color;
+	this.coins = generateCoins(color);
+	this.kill = 0;
+}
+
+Player.prototype = {
+	hasAKill : function(){
+		
+	}
+};
+
+exports.Player = Player;
