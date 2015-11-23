55c81a2db644fba208a497e27b5f92aaf95166e0
vathsala <vathsala@INvathsala.local>
2015-11-23 12:27:54 +0530

    coin moving function is deleted ... The coin move just updates the position of coin

--- library/coinLogic.js
+++ library/coinLogic.js
@@ -1,28 +1,13 @@
-var board = require('./board.js').module;
-var lib = {};
-exports.lib = lib;
-
-var coinMoving = function(player,currentPosition,finalPosition){
-	var currentIndex = board.routes.player.indexOf(currentPosition);
-	var finalIndex = board.routes.player.indexOf(finalPosition);
-	var path = [];
-	for(var i = currentIndex ; i<finalIndex; i++){
-		path.push(player[i])
-	}
-	return path;
-}
-
-lib.Coin = function(colour,id){
+exports Coin = function(colour,id){
 	this.colour = colour;
 	this.position = [0,0];
 	this.id = id;
 }
 
-lib.Coin.prototype = {
+Coin.prototype = {
 	previousPosition : [0,0],
 	move : function(finalPosition){
 		this.previousPosition = this.position;
-		this.path = coinMoving(this.colour,this.position,finalPosition);
 		this.position = finalPosition;
 	},
 	die : function(){
