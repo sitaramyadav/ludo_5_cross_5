979662359013f7b2e32cd8578b9acc7c659cc684
sitaram <sitaram@thoughtworks.com>
2016-02-15 22:52:44 +0530

    changed the port because of port error while hosting.
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var controller = require('./library/controller.js');
 var server = http.createServer(controller);
-server.listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||8080,process.env.OPENSHIFT_NODEJS_IP);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||8080||4000||3333||6666,process.env.OPENSHIFT_NODEJS_IP);
 console.log(server.on('error',function (argument) {
 	console.log(argument,'====argument');
 }),'===========');
